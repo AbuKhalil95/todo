8761a7c0fd0f6fab7283ff1f20f518917513cf0a
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-10-22 08:48:14 +0300

    proposed structure

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 import ToDo from './components/todo/todo.js';
 import SiteContext from './context/site.js';
+import { AuthContext } from './context/auth.js';
+import Login from './components/login/login';
 
 export default () => {
+    const { context } = useContext(AuthContext);
+    let okToRender = false; 
+
+    try {
+        okToRender = context.loggedIn
+    } catch(e) {
+        console.log("error in Auth component !")
+    }
+
     return (
     <SiteContext>
-        <ToDo />
+        <If condition={okToRender}>
+            <Then>
+                <ToDo />
+            </Then>
+        </If>
+        <Else>
+            <When condition={!okToRender}>
+                <Login />
+            </When>
+        </Else>
     </SiteContext>
     );
 }
